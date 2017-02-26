a0fffaf9ef9852261909053e8fe2f0484870c478
Dylan Andrews <dylanandr@gmail.com>
2017-02-26 12:33:58 -0600

    Set up posts controller for creation and showing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
+  end
 
+  def new
   end
 
   def index
+    @posts = Posts.all
+  end
 
+  def create
+    byebug
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
